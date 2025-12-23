b6c5cc656b743132896b677e11221cdfa4032bc7
Cecer <cecer_git@cecer1.com>
2025-12-23 15:26:00 +0000

    Update to new alert formats

--- src/Config.cs
+++ src/Config.cs
@@ -4,11 +4,11 @@ public class Config
 {
     public readonly Alert[] Alerts =
     [
-        new("""^<font color="red">\[Restart\]</font> Server restart in 15 minute\(s\)!.*$""", "Restart in 15 minutes", "game:sounds/effect/deepbell"),
-        new("""^<font color="red">\[Restart\]</font> Server restart in 10 minute\(s\)!.*$""", "Restart in 10 minutes", "game:sounds/effect/deepbell"),
-        new("""^<font color="red">\[Restart\]</font> Server restart in 5 minute\(s\)!.*$""", "Restart in 5 minutes", "game:sounds/effect/deepbell"),
-        new("""^<font color="red">\[Restart\]</font> Server restart in 3 minute\(s\)!.*$""", "Restart in 3 minutes", "game:sounds/effect/deepbell"),
-        new("""^<font color="red">\[Restart\]</font> Server restart in 1 minute\(s\)!.*$""", "Restart in 1 minutes", "game:sounds/effect/deepbell"),
+        new("""^<font color="red">\[Restart\]</font> Server restart in roughly 15 minute\(s\)!.*$""", "Restart in about 15 minutes", "game:sounds/effect/deepbell"),
+        new("""^<font color="red">\[Restart\]</font> Server restart in roughly 10 minute\(s\)!.*$""", "Restart in about 10 minutes", "game:sounds/effect/deepbell"),
+        new("""^<font color="red">\[Restart\]</font> Server restart in roughly 5 minute\(s\)!.*$""", "Restart in about 5 minutes", "game:sounds/effect/deepbell"),
+        new("""^<font color="red">\[Restart\]</font> Server restart in roughly 3 minute\(s\)!.*$""", "Restart in about 3 minutes", "game:sounds/effect/deepbell"),
+        new("""^<font color="red">\[Restart\]</font> Server restarting NOW!.*$""", "RESTARTING NOW", "game:sounds/effect/deepbell"),
         new("^A (light) temporal storm is (approaching)$", "Storm Approaching (light)", "game:sounds/effect/deepbell"),
         new("^A (medium) temporal storm is (approaching)$", "Storm Approaching (medium)", "game:sounds/effect/deepbell"),
         new("^A (heavy) temporal storm is (approaching)$", "Storm Approaching (heavy)", "game:sounds/effect/deepbell"),
