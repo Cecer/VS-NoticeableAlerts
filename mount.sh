4f38dae332fdc5e1390761539093eafbd12d916f
Cecer <cecer_git@cecer1.com>
2026-03-03 06:52:09 +0000

    Update mount.sh (prevent duplicate mounts)

--- mount.sh
+++ mount.sh
@@ -9,6 +9,16 @@ project_name="${project_name%%.csproj}";
 src_path="$project_dir/builds/$project_name";
 dest_path="/home/chris/.var/app/at.vintagestory.VintageStory/config/VintagestoryData/Mods/$project_name";
 
+is_mounted() {
+    path="$1";
+    cat /proc/mounts | cut -d " " -f 2 | grep "^$path\$" > /dev/null
+}
+
+if is_mounted "$dest_path"; then
+    printf "Already mounted!" >&2;
+    exit 1;
+fi
+
 [[ -d "$dest_path" ]] || mkdir "$dest_path";
 
 "$project_dir/build.sh";
